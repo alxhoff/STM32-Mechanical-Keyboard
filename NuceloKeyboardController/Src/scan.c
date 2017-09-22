de9fadee1f6ebe10dc6628c6454f18523e1a4ebc
Alex Hoffman <alxhoff@gmail.com>
2017-09-22 20:38:49 +0200

    [MACRO] macro set input adapter to use shift registers

--- NuceloKeyboardController/Src/scan.c
+++ NuceloKeyboardController/Src/scan.c
@@ -29,7 +29,6 @@ key_err_t scan_key_matrix(keyboard_device_t* keyboard_dev, keyboard_HID_data_t* 
 
 	for(uint8_t row=0;row<KEYBOARD_ROWS;row++){
 		//Set current column high so that rows can be read
-//		HAL_GPIO_WritePin(row_ports[row],row_pins[row], GPIO_PIN_SET);
 		row_mask = (1<<(7-row));
 		shift_array->set_byte(shift_array, 0, row_mask);
 		shift_array->output(shift_array, 1);
@@ -42,10 +41,8 @@ key_err_t scan_key_matrix(keyboard_device_t* keyboard_dev, keyboard_HID_data_t* 
 				HID_reports->key_buf.index++;
 			}
 		}
-//		HAL_GPIO_WritePin(row_ports[row],row_pins[row], GPIO_PIN_RESET);
 		shift_array->set_byte(shift_array, 0, 0x00);
 		shift_array->output(shift_array, 1);
-		HAL_Delay(1);
 	}
 
 	if(HID_reports->key_buf.index == 0)
@@ -64,7 +61,6 @@ key_code scan_get_single_key( keyboard_device_t* keyboard_dev, keymap_list_t* la
 	while(ret == 0){
 		for(uint8_t row=0;row<KEYBOARD_ROWS;row++){
 			//Set current column high so that rows can be read
-//			HAL_GPIO_WritePin(keyboard_dev->row_ports[row],keyboard_dev->row_pins[row], GPIO_PIN_SET);
 			row_mask = (1<<(7-row));
 			GET_SHIFT_DEVICE->set_byte(GET_SHIFT_DEVICE, 0, row_mask);
 			GET_SHIFT_DEVICE->output(GET_SHIFT_DEVICE, 1);
@@ -75,10 +71,8 @@ key_code scan_get_single_key( keyboard_device_t* keyboard_dev, keymap_list_t* la
 					//display_int_on_screen(col, row);
 				}
 			}
-//			HAL_GPIO_WritePin(keyboard_dev->row_ports[row],keyboard_dev->row_pins[row], GPIO_PIN_RESET);
 			GET_SHIFT_DEVICE->set_byte(GET_SHIFT_DEVICE, 0, 0x00);
 			GET_SHIFT_DEVICE->output(GET_SHIFT_DEVICE, 1);
-			HAL_Delay(1);
 		}
 	}
 	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
@@ -96,6 +90,7 @@ char* scan_get_input_seq( keyboard_device_t* keyboard_dev, keymap_list_t* list )
 	input_str = (char*)malloc(sizeof(char));
 
 	uint8_t finished = 0;
+	static uint8_t row_mask = 0x00;
 
 	//debounce stuff
 	uint8_t button_input[KEYBOARD_ROWS][KEYBOARD_COLS] = {0};
@@ -109,7 +104,11 @@ char* scan_get_input_seq( keyboard_device_t* keyboard_dev, keymap_list_t* list )
 	while(!finished){
 		//FOR
 		for(uint8_t row=0;row<KEYBOARD_ROWS;row++){
-			HAL_GPIO_WritePin(keyboard_dev->row_ports[row],keyboard_dev->row_pins[row], GPIO_PIN_SET);
+
+			row_mask = (1<<(7-row));
+			GET_SHIFT_DEVICE->set_byte(GET_SHIFT_DEVICE, 0, row_mask);
+			GET_SHIFT_DEVICE->output(GET_SHIFT_DEVICE, 1);
+
 			for(uint8_t col=0;col<KEYBOARD_COLS;col++){
 
 				button_input[row][col] = !HAL_GPIO_ReadPin(keyboard_dev->col_ports[col], keyboard_dev->col_pins[col]);
@@ -121,7 +120,6 @@ char* scan_get_input_seq( keyboard_device_t* keyboard_dev, keymap_list_t* list )
 					if( button_input[row][col] != button_cur_state[row][col]){
 						button_cur_state[row][col] = button_input[row][col]; // <---
 						if(!button_input[row][col]){
-							//push
 							if(str_size%8 <= 4){
 								input_str = (char*)realloc(input_str, sizeof(char) * (str_size + 8));
 								if(!input_str) return NULL;
@@ -148,15 +146,15 @@ char* scan_get_input_seq( keyboard_device_t* keyboard_dev, keymap_list_t* list )
 						}else{
 							input_char = current_layer->grid[row][col];
 							if(input_char == 0xE1 || input_char == 0xE5)
-									shift_modifier &= ~(1 << input_char - 0xE0);
+									shift_modifier &= ~(1 << (input_char - 0xE0));
 							input_char = 0;
 						}
 					}
 				}
 				button_last_state[row][col] = button_input[row][col];
 			}
-			HAL_GPIO_WritePin(keyboard_dev->row_ports[row],keyboard_dev->row_pins[row], GPIO_PIN_RESET);
-			HAL_Delay(1);
+			GET_SHIFT_DEVICE->set_byte(GET_SHIFT_DEVICE, 0, 0x00);
+			GET_SHIFT_DEVICE->output(GET_SHIFT_DEVICE, 1);
 		}
 	}
 	input_str[str_size++]='\0';
