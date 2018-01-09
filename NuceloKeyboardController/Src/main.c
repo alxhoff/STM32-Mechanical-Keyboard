fd253e129ec5e66a9f3da95ef94163a07fc60849
Alex Hoffman <alxhoff@gmail.com>
2018-01-09 22:22:09 +0100

    Status LEDS tested working

--- NuceloKeyboardController/Src/main.c
+++ NuceloKeyboardController/Src/main.c
@@ -164,11 +164,23 @@ int main(void)
 	//TODO STANDARDIZE DEVICE INIT
 	shift_array_t shift_array =
 			{ .dev_count = 1,
-					.ser_in_pin = GPIO_PIN_15,	//GPIO PINS FOR SHIFT ARRAY
-					.ser_in_port = GPIOB, .ser_in_clock_init = 1, .ser_clk_pin =
-							GPIO_PIN_15, .ser_clk_port = GPIOA,
-					.ser_clk_clock_init = 1, .latch_pin = GPIO_PIN_12,
-					.latch_port = GPIOB, .latch_clock_init = 1 };
+					.ser_in_pin = GPIO_PIN_1,	//GPIO PINS FOR SHIFT ARRAY
+					.ser_in_port = GPIOB,
+					.ser_in_clock_init = 1,
+					.ser_clk_pin = GPIO_PIN_8,
+					.ser_clk_port = GPIOF,
+					.ser_clk_clock_init = 1,
+					.latch_pin = GPIO_PIN_3,
+					.latch_port = GPIOE,
+					.latch_clock_init = 1,
+					.out_ena_pin = 9,
+					.out_ena_port = GPIOF,
+					.out_ena_clock_init = 1,
+					.sr_clr_pin = 7,
+					.sr_clr_port = GPIOF,
+					.sr_clr_clock_init = 1,
+			};
+
 
 	shift_init(keyboard_devs, &shift_array);
 
@@ -577,6 +589,37 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  //test
+
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+
 }
 
 /* USER CODE BEGIN 4 */
@@ -656,6 +699,11 @@ void KeyboardListenCallback(void const * argument)
 	for (;;)
 	{
 		vTaskDelayUntil(&xLastWakeTime, xPeriod);
+
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_SET);
 
 		if (scan_key_matrix(keyboard_devs->keyboard,
 				keyboard_devs->keyboard_HID, keyboard_devs->shift_array) == 0)
